898f14d2e20db5119be61013440d1b211c8b00b4
zhangjiaran <zhangjiaran@zhangjiaranai-db699-167513-worker-0.zhangjiaran.basemind-core.svc.platform.basemind.local>
2024-04-15 17:25:11 +0800

    整理sample.py文件

--- sample.py
+++ sample.py
@@ -1,23 +1,25 @@
 import torch
 import matplotlib.pyplot as plt
 import click
+from loguru import logger
 from pathlib import Path
 
 from model import *
 from model import BigramLanguageModel
 
-@click.command()
-@click.option("--model_path",type=Path,default='model.pth')
+# python sample.py --model_path --num
+
+
 def load_model(model_path:Path):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    logger.info("Loading weights")
     model = torch.load(model_path)
     model=model.to(device)
     model.eval()
+    return model
 
 
-@click.command()
-@click.option("--num",type=int,default='10')
 def get_sample(num:int):
     all_sample = torch.tensor(images, dtype=torch.long)
     sample=all_sample[-num:]
@@ -27,11 +29,12 @@ def get_sample(num:int):
 
     return half_sample
 
-def show_images(result_tensor,num:int):
+def save_images(result_tensor,num:int):
+    print("begin")
     result_reshape=result_tensor.view(-1,28,28)
 
     # 创建一个网格以显示多个图像
-    fig, axes = plt.subplots(nrows=num/5, ncols=5, figsize=(10, 5))
+    fig, axes = plt.subplots(nrows=int(num/5), ncols=5, figsize=(10, 5))
 
     # 将张量数据转化为numpy后显示
     for i, ax in enumerate(axes.flatten()):
@@ -42,12 +45,17 @@ def show_images(result_tensor,num:int):
     plt.savefig('image.png')
 
 
+@click.command()
+@click.option("--model_path",type=Path,default='model.pth')
+@click.option("--num",type=int,default=10)
+def main(model_path:Path,num:int):
+    model=load_model(model_path)
 
-def main():
-    load_model()
-    half_sample=get_sample()
+    half_sample=get_sample(num)
+
+    logger.info("generating中")
     result_tensor=model.generate(half_sample,max_new_tokens=392)
-    show_images(result_tensor)
+    save_images(result_tensor,num)
 
 
 if __name__ == "__main__":
