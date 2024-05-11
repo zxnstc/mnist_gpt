b51a657464ab41d36b9784f810dd719173ac5377
zhangjiaran <zhangjiaran@zhangjiaranai.zhangjiaran.basemind-core.svc.platform.basemind.local>
2024-05-11 15:47:34 +0800

    sample文件图片显示格式format

--- sample.py
+++ sample.py
@@ -4,11 +4,30 @@ import click
 from loguru import logger
 from pathlib import Path
 import os
+import pickle
 
 from model import *
 from model import BigramLanguageModel
 
 # python sample.py --model_path --num
+
+def load_data():
+    def load_dict_from_file(filename):
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
+    data_dict = load_dict_from_file('/data/mnist_gpt/data/test_data_sorted.pkl')
+    sample_list=[]
+    for label,image_list in data_dict.items():
+        for i in range(10):
+            image=image_list[i]
+            image[0]=256
+            sample_list.append(image)
+
+    result_tensor=torch.stack(sample_list)
+    print(result_tensor.shape)
+    return result_tensor
+    
+            
 
 def get_image_path(model_path:Path)->Path:
     head, file_name = os.path.split(model_path)
@@ -34,19 +53,22 @@ def load_model(model_path:Path):
 
 
 def get_sample(num:int):
-    all_sample = torch.tensor(images, dtype=torch.long)
-    sample=all_sample[-num:]
+    # all_sample = torch.tensor(images, dtype=torch.long)
+    sample=load_data() #torch.Size([100, 785])
     # 切片之后的sample
-    half_sample = sample[:, :392]
+    half_sample = sample[:, :588]
+    # BOT_sample=torch.full((half_sample.size(0), 1), 256)
+    # half_sample=torch.cat((BOT_sample,half_sample),dim=1)
+    print("half_s",half_sample.shape,half_sample[0][0])
     half_sample=half_sample.to(device)
 
     return half_sample
 
 def save_images(result_tensor,num:int,model_path:Path):
-    result_reshape=result_tensor.view(-1,28,28)
+    result_reshape=result_tensor[:,1:].view(-1,28,28)
 
     # 创建一个网格以显示多个图像
-    fig, axes = plt.subplots(nrows=int(num/5), ncols=5, figsize=(10, 5))
+    fig, axes = plt.subplots(nrows=int(num/10), ncols=10, figsize=(10, 10))
 
     # 将张量数据转化为numpy后显示
     for i, ax in enumerate(axes.flatten()):
@@ -63,14 +85,14 @@ def save_images(result_tensor,num:int,model_path:Path):
 
 @click.command()
 @click.option("--model_path",type=Path)
-@click.option("--num",type=int,default=10)
+@click.option("--num",type=int,default=100)
 def main(model_path:Path,num:int):
     model=load_model(model_path)
 
     half_sample=get_sample(num)
 
     logger.info("generating中")
-    result_tensor=model.generate(half_sample,max_new_tokens=392)
+    result_tensor=model.generate(half_sample,max_new_tokens=197)
     save_images(result_tensor,num,model_path)
 
 
